6ba3b9a0895dced95aeacae4449413f76499eafe
Jakub Szklarz <kubusisko1@gmail.com>
2024-12-04 16:34:57 +0100

    API for roles

---
+++ CeramikaAPI/CeramikaAPI/Controllers/RoleController.cs
@@ -0,0 +1,58 @@
+﻿using CeramikaAPI.Models;
+using CeramikaAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CeramikaAPI.Controllers
+{
+    public class RoleForm
+    {
+        public string RoleName { get; set; }
+    }
+
+    public class RoleSetForm
+    {
+        public int RoleId { get; set; }
+        public int UserId { get; set; }
+    }
+
+    [Route("api/roles")]
+    [ApiController]
+    public class RoleController : ControllerBase
+    {
+        private readonly RoleService roleService;
+        public RoleController() { roleService = new RoleService(); }
+
+        [HttpGet]
+        [ProducesResponseType<List<RoleModel>>(StatusCodes.Status200OK)]
+        public IActionResult GetRoles()
+        {
+            return Ok(roleService.GetRoles());
+        }
+
+        [HttpPost("createRole")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult CreateRole([FromForm] RoleForm roleForm)
+        {
+            RoleModel model = new RoleModel { Name = roleForm.RoleName };
+            RoleModel role = roleService.AddRole(model);
+            return role == null ? BadRequest() : Ok(role);
+        }
+
+        [HttpGet("getUserRoles")]
+        [ProducesResponseType<List<UserRolesModelDTO>>(StatusCodes.Status200OK)]
+        public IActionResult GetUserRoles()
+        {
+            return Ok(roleService.GetUserRoles());
+        }
+
+        [HttpPost("setRole")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult CreateRole([FromForm] RoleSetForm roleSetForm)
+        {
+            UserRolesModelDTO model = roleService.SetRole(roleSetForm.RoleId, roleSetForm.UserId);
+            return model == null ? BadRequest() : Ok(model);
+        }
+    }
+}
---
+++ CeramikaAPI/CeramikaAPI/Services/RoleService.cs
@@ -0,0 +1,56 @@
+﻿using CeramikaAPI.Context;
+using CeramikaAPI.Models;
+
+namespace CeramikaAPI.Services
+{
+    public class RoleService
+    {
+        private CeramikaContext context;
+        public RoleService() { context = new CeramikaContext(); }
+
+        public RoleModel? AddRole(RoleModel role)
+        {
+            context.Roles.Add(role);
+            try { context.SaveChanges(); } catch { return null; }
+            return role;
+        }
+
+        public RoleModel? roleById(int id)
+        {
+            try
+            {
+                return context.Roles.First(x => x.Id == id);
+            }
+            catch { return null; }
+        }
+
+        public UserRolesModelDTO? SetRole(int roleId, int userId)
+        { 
+            UserService userservice = new UserService();
+            UserModel user = userservice.UserById(userId);
+            RoleModel role = roleById(roleId);
+            context.Attach(user);
+            context.Attach(role);
+            context.Add( new UserRolesModel { Role = role, User = user });
+            try { context.SaveChanges(); } catch { return null;}
+            return new UserRolesModelDTO { User = user.Name, Role = role.Name };
+        }
+
+        public List<UserRolesModelDTO>? GetUserRoles()
+        {
+            try
+            {
+                return context.UserRoles.Select(c => new UserRolesModelDTO
+                {
+                    Role = c.Role.Name,
+                    User = c.User.Name
+                }).ToList();
+            } catch { return null; }
+        }
+
+        public List<RoleModel>? GetRoles()
+        {
+            try { return context.Roles.ToList(); } catch { return null; }
+        }
+    }
+}
